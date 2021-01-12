915bd285a5150a4d5fd561ce8f7bc1682bb8cdca
Lennart Reimann <lennart.reimann@ice.rwth-aachen.de>
2021-01-12 18:17:07 +0100

    [FORMAL][OVERALL] Improved the CI yaml script to check whether the test cases are completed successfully.

--- formal/example-from-issue639-check-results.sh
+++ formal/example-from-issue639-check-results.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
-
+return_value=0
 for i in example-from-issue639-*/;
 do
   pushd $i
   echo ""
+
   if grep -ni 'Equivalence successfully proven!' equiv.log
   then
-    echo -e "\nACTUAL:   Equivalence successfully proven!"
+    actual_output_string='ACTUAL:   Equivalence successfully proven!'
   else
-    echo -e "\nACTUAL:   Equivalence **__NOT__** proven!"
+    actual_output_string='ACTUAL:   Equivalence **__NOT__** proven!'
   fi
-  cat expected-equiv.result
+
+  echo -e $actual_output_string
+
+  expected_output_string=`cat expected-equiv.result`
+  echo -e $expected_output_string
+
+  if [ "${actual_output_string:10:30}" != "${expected_output_string:10:30}" ]
+  then
+    return_value=1
+  fi
+
   echo ""
   popd
 done
+exit $return_value
